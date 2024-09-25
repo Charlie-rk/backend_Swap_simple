5204df89cda39fdc5a9b4d5c9e0f84edbf955025
Charlie-rk <134798859+Charlie-rk@users.noreply.github.com>
2024-09-25 23:50:47 +0530

    Update pnrController.js

--- controllers/pnrController.js
+++ controllers/pnrController.js
@@ -1,31 +1,32 @@
-
-import axios from 'axios'
+import axios from 'axios';
 
 class PNRController {
   constructor(apiKey) {
     this.apiKey = apiKey;
-    this.apiUrl = 'https://pnr-status-indian-railway.p.rapidapi.com';
+    this.apiUrl = 'https://irctc-indian-railway-pnr-status.p.rapidapi.com'; // Updated API URL
     this.headers = {
-      'X-RapidAPI-Key': this.apiKey,
-      'X-RapidAPI-Host': 'pnr-status-indian-railway.p.rapidapi.com'
+      'x-rapidapi-key': this.apiKey,  // Changed the header key
+      'x-rapidapi-host': 'irctc-indian-railway-pnr-status.p.rapidapi.com'  // Updated the host
     };
   }
 
   async getPNRStatus(pnr) {
     const options = {
       method: 'GET',
-      url: `${this.apiUrl}/pnr-check/${pnr}`,
+      url: `${this.apiUrl}/getPNRStatus/${pnr}`,  // Updated endpoint
       headers: this.headers
     };
 
     try {
       const response = await axios.request(options);
+      // console.log("Here let See!!");
+      // console.log(response.data);
       return response.data;
     } catch (error) {
+      console.error('Error fetching PNR status:', error);
       throw error;
     }
   }
 }
 
 export default PNRController;
-// module.exports = PNRController;
